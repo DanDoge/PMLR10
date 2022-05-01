d309ec8faf6738b7e7f2b33897faba602f3302c6
zhangjiahui-buaa <17231043@buaa.edu.cn>
2022-05-01 16:56:59 +0200

    add methods to compute selectivity and importance

--- model.py
+++ model.py
@@ -1,33 +1,84 @@
-import torch 
+import torch
 import torchvision
 import torch.nn as nn
 import torchvision.transforms as transforms
 
+
 class ConvNet(nn.Module):
-    def __init__(self, in_channel = 3, activ = nn.functional.relu):
+    def __init__(self, in_channel=3, activ=nn.functional.relu, num_classes=10):
         super(ConvNet, self).__init__()
         self.activ = activ
         self.in_channel = in_channel
-
-        layer_sizes = [64, 64, 128, 128, 128, 256, 256, 256, 512, 512, 512]
+        self.num_classes = num_classes
+        self.layer_sizes = [64, 64, 128, 128, 128, 256, 256, 256, 512, 512, 512]
+        self.feature_sizes = [32, 32, 16, 16, 16, 8, 8, 8, 4, 4, 4]
         strides = [1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1]
         # here we have to assume for all layers, padding = 1, otherwise not trainable
         self.conv_layers = [
-            torch.nn.Conv2d(in_channel, layer_sizes[0], kernel_size=3, stride=strides[0], # specified in the appendix
-            padding=1, dilation=1, groups=1, bias=True, padding_mode='zeros', device=None, dtype=None), # not specififed params here
+            torch.nn.Conv2d(in_channel, self.layer_sizes[0], kernel_size=3, stride=strides[0],
+                            # specified in the appendix
+                            padding=1, dilation=1, groups=1, bias=True),  # not specififed params here
         ]
-        for i in range(1, len(layer_sizes)):
+        for i in range(1, len(self.layer_sizes)):
             self.conv_layers.append(
-                torch.nn.Conv2d(layer_sizes[i-1], layer_sizes[i], kernel_size=3, stride=strides[i], padding = 1)
+                torch.nn.Conv2d(self.layer_sizes[i - 1], self.layer_sizes[i], kernel_size=3, stride=strides[i],
+                                padding=1)
             )
 
         self.conv_layers = nn.Sequential(*self.conv_layers)
-        self.fc = torch.nn.Linear(512*4*4, 10)
+        self.fc = torch.nn.Linear(512 * 4 * 4, 10)
 
         print("---network architecture---")
         print(self.conv_layers)
         print("--------------------------")
-        
+
+    def init_mus(self):
+        self.mus = [torch.zeros((self.num_classes, self.layer_sizes[0]))]  # num_classes * layer_size
+        for i in range(1, len(self.layer_sizes)):
+            self.mus.append((torch.zeros((self.num_classes, self.layer_sizes[i]))))
+
+    def forward_and_record(self, x, labels):
+        for layer, mu in zip(self.conv_layers, self.mus):
+            x = layer(x)
+            x = self.activ(x)  # bs * layer_size * d * d
+            avg_x = torch.mean(x, (2, 3))  # bs * layer_size, activity of feature map is averaged across all elemnts
+            for i, single in enumerate(avg_x):
+                mu[labels[i]] += single
+
+        x = torch.flatten(x, start_dim=1)
+        x = self.fc(x)
+        return x
+
+    def compute_selectivity(self, testloader, device):
+        num_images = 0
+        self.init_mus()
+        with torch.no_grad():
+            for data in testloader:
+                images, labels = data[0].to(device), data[1].to(device)
+                num_images += images.shape[0]
+                _ = self.forward_and_record(images, labels)
+        result = []
+        for mu in self.mus:
+            t_mu = torch.transpose(mu, 0, 1)
+            mu_max, _ = torch.max(t_mu, 1)
+            mu_sum = torch.sum(t_mu, 1)
+            mu_mmax = (mu_sum - mu_max) / (self.num_classes - 1)
+            mu_max /= num_images
+            mu_mmax /= num_images
+            result.append(torch.div(mu_max-mu_mmax, mu_max+mu_mmax))
+        return result
+
+    def clamp_forward(self, x, layer_idx, neuron_idx):  # clamp jth neuron on ith layer
+        for i, layer in enumerate(self.conv_layers):
+            x = layer(x)
+            x = self.activ(x)
+            if i == layer_idx:
+                for single in x:
+                    single[neuron_idx] = torch.zeros((self.feature_sizes[layer_idx], self.feature_sizes[layer_idx]))
+        x = torch.flatten(x, start_dim=1)
+        x = self.fc(x)
+
+        return x
 
     def forward(self, x):
         for layer in self.conv_layers:
--- train.py
+++ train.py
@@ -1,12 +1,12 @@
 # https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
 
-import torch 
+import torch
 import torchvision
 import torch.nn as nn
 import torchvision.transforms as transforms
-
+import os
 from model import ConvNet
-        
+
 transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
@@ -21,32 +21,44 @@ testset = torchvision.datasets.CIFAR10(root='./data', train=False,
 testloader = torch.utils.data.DataLoader(testset, batch_size=32,
                                          shuffle=False, num_workers=0)
 
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print(device)
 net = ConvNet()
+net.to(device)
 optimizer = torch.optim.Adam(net.parameters(), lr=1e-3)
 criterion = nn.CrossEntropyLoss()
 
-epoch = 100 # in the paper
+epoch = 0  # in the paper
+test_loss = 0
+if not os.path.exists("log"):
+    os.mkdir("log")
 for i in range(epoch):
-    for batch in trainloader:
-        inputs, labels = batch
+    total_loss = 0
+    for j, batch in enumerate(trainloader):
+        inputs, labels = batch[0].to(device), batch[1].to(device)
 
         optimizer.zero_grad()
         outputs = net(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
+        total_loss += loss.item()
         optimizer.step()
 
-        print(loss.item())
-
+        if j % 200 == 199:  # print every 2000 mini-batches
+            print(f'[{i + 1}, {j + 1:5d}] loss: {total_loss / 200:.3f}')
+            total_loss = 0.0
 
     correct = 0
     total = 0
     # since we're not training, we don't need to calculate the gradients for our outputs
     with torch.no_grad():
+        test_loss = 0
         for data in testloader:
-            images, labels = data
+            images, labels = data[0].to(device), data[1].to(device)
             # calculate outputs by running images through the network
             outputs = net(images)
+            loss = criterion(outputs, labels)
+            test_loss += loss.item()
             # the class with the highest energy is what we choose as prediction
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
@@ -56,3 +68,22 @@ for i in range(epoch):
 
     PATH = './log/convnet_{}.pth'.format(i)
     torch.save(net.state_dict(), PATH)
+
+
+selectivity = net.compute_selectivity(testloader, device) # a list of selectivity index of neurons on all layers
+importance = []
+for i, sel in enumerate(selectivity):
+    importance.append([])
+    for j, val in enumerate(sel):
+        now_loss = 0
+        with torch.no_grad():
+            for data in testloader:
+                images, labels = data[0].to(device), data[1].to(device)
+                # calculate outputs by running images through the network
+                outputs = net.clamp_forward(images, i, j) # use clamp_forward instead of forward
+                loss = criterion(outputs, labels)
+                now_loss += loss.item()
+
+        importance[-1].append(now_loss-test_loss)
+
+# TODO :draw figure of importance to selectivity
